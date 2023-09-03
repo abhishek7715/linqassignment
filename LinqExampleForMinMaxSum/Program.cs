727c9028f106613137b68e666ad874244af3db84
abhi7715 <abhishekp3103@gmail.com>
2023-09-03 22:37:26 +0530

    commiting 1st

---
+++ LinqExampleForMinMaxSum/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace LinqExampleForMinMaxSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] numbers = { "10", "5", "100", "30", "50", "abc", "pqr" };
+
+            var integers = numbers.Where(n => int.TryParse(n, out int v));
+
+            int maxNumber=integers.Select(n => int.Parse(n)).Max();
+            Console.WriteLine($"Max Number: {maxNumber}");
+            int secondMaxNumber = integers.Select(n => int.Parse(n)).Where(n => n < maxNumber).Max();
+            Console.WriteLine($"Second Max Number: {secondMaxNumber}");
+          
+            int minNumber = integers.Select(n => int.Parse(n)).Min();
+           
+            Console.WriteLine($"Min Number: {minNumber}");
+            int sum = integers.Select(n => int.Parse(n)).Sum();
+            Console.WriteLine($"Sum of Numbers: {sum}");
+
+            Console.Write("Descending : ");
+            integers.Select(n => int.Parse(n)).OrderByDescending(n => n).ToList().ForEach(n=>Console.Write(n+" "));
+
+        }
+    }
+}
